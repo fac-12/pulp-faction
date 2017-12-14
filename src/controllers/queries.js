f27a7d64ab9c0649615f9a54afe300a046ba0964
sophielevens <sophielevens1@gmail.com>
2017-12-14 15:00:20 +0000

    signup backend is working

--- src/controllers/queries.js
+++ src/controllers/queries.js
@@ -1,11 +1,14 @@
 const db = require('../database/db_connections');
 
 const checkUser = gitterhandle => {
-  console.log(gitterhandle);
-  return db.query(`SELECT CASE WHEN EXISTS(SELECT gitterhandle FROM users WHERE gitterhandle = $1) THEN CAST (true AS BOOLEAN) ELSE CAST (false AS BOOLEAN) END`, [gitterhandle])
+  return db.query('SELECT CASE WHEN EXISTS(SELECT gitterhandle FROM users WHERE gitterhandle = $1) THEN CAST (true AS BOOLEAN) ELSE CAST (false AS BOOLEAN) END', [gitterhandle])
 };
 
+const createUser = (name, gitterhandle, hashedPw) => {
+  return db.query('INSERT INTO users(name, gitterhandle, password) VALUES ($1, $2, $3) RETURNING id, gitterhandle, name', [name, gitterhandle, hashedPw]);
+};
 
 module.exports = {
   checkUser,
+  createUser,
 }
