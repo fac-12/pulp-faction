75194b4af400bd813379c79b07187d69388552b9
trc5019 <trc5019@gmail.com>
2017-12-14 16:57:38 +0000

    display books on library page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,17 +2,17 @@ const router = require('express').Router();
 const path = require('path');
 const error = require('./error');
 const home = require('./home');
-const singleBook = require('./singleBook');
 const signUp = require('./signUp');
 const login = require('./login');
 const library = require('./library');
+const id = require('./id');
 
 router.get('/', home.get);
 
 router.post('/login', login.post);
 router.post('/signup', signUp.post);
 router.get('/library', library.get);
-router.get('/library/:singleBook', singleBook.get);
+router.get('/library/:id', id.get);
 
 router.use(error.client);
 
--- src/controllers/singleBook.js
+++
@@ -1,8 +0,0 @@
-
-exports.get = (req, res, next) => {
-  const { singleBook } = req.params;
-  // we need to do a query to db
-  // if (library.includes(singleBook)) {
-  //   return res.render('singleBook', { singleBook });
-  // }
-}
