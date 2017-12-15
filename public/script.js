586e596c395d62aa147986f4293f3817c1297d14
Mohamed Osman <trc5019@gmail.com>
2017-12-15 12:32:08 +0000

    Update script.js

--- public/script.js
+++ public/script.js
@@ -43,7 +43,7 @@ signupForm.addEventListener('submit', function(event) {
   var body = {
     name: suName.value,
     gitterhandle: suGitterHandle.value,
-    password: suPw.value
+    password: suPw.value,
   };
 
   xhrTemplate('/signup', 'POST', body, 201, function(response) {
